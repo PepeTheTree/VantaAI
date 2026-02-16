acf65aa4992407961322d12fe1cc72545d92648e
Vanta Deploy <deploy@vantaai.dev>
2026-02-16 15:51:58 -0800

    Fix: Update API URL to chat.vantaai.dev

---
+++ assets/browserAll-CsB3MTc2.js
@@ -0,0 +1,1 @@
+import{e as i,A as e,C as a,a as s,E as t,F as n}from"./index-BgWYn8kY.js";import"./webworkerAll-BLbcVLY2.js";i.add(e);i.mixin(a,s);i.add(t);i.mixin(a,n);
--- assets/webworkerAll-DS0FPETp.js
+++
@@ -1,1 +0,0 @@
-import{e,b as s,c as a}from"./index-q156OVPJ.js";e.add(s);e.add(a);
